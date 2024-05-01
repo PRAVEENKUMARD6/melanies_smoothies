03583f9966c4506fe2235841e0f2d3768cd74d24
PRAVEENKUMARD6 <168643957+PRAVEENKUMARD6@users.noreply.github.com>
2024-05-01 20:20:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,6 +45,6 @@ if ingredients_list:
 
 #New section to display fruitvice nutrition information
 import requests
-fruitvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 #st.text(fruitvice_response.json)
 fv_df = st.dataframe(data=fruityvice_response.josn(),use_container_width=True)
